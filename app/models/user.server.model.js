30b118b0ee177e43d671b8eba688036bd14dde63
moeabdol <mohd.a.saed@gmail.com>
2017-08-12 20:37:22 +0300

    Add 'created' field to UserSchema

--- app/models/user.server.model.js
+++ app/models/user.server.model.js
@@ -1,12 +1,18 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+mongoose.Promise = global.Promise;
+
 const UserSchema = new Schema({
   firstName:  String,
   lastName:   String,
   email:      String,
   username:   String,
-  password:   String
+  password:   String,
+  created: {
+    type:     Date,
+    default:  Date.now
+  }
 });
 
 mongoose.model("User", UserSchema);
