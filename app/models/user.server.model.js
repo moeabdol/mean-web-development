cbc2bb31f45706deebd58906cb8c5512f35b20bc
moeabdol <mohd.a.saed@gmail.com>
2017-08-12 21:19:47 +0300

    Add virtual attribute 'fullName' and trim 'username'

--- app/models/user.server.model.js
+++ app/models/user.server.model.js
@@ -7,12 +7,38 @@ const UserSchema = new Schema({
   firstName:  String,
   lastName:   String,
   email:      String,
-  username:   String,
+  username: {
+    type:     String,
+    trim:     true
+  },
   password:   String,
   created: {
     type:     Date,
     default:  Date.now
+  },
+  website: {
+    type:     String,
+    get: (url) => {
+      if (!url) {
+        return url;
+      } else {
+        if (url.indexOf("http://") !== 0 && url.indexOf("https://") !== 0) {
+          url = "http://" + url;
+        }
+        return url;
+      }
+    }
   }
 });
+
+UserSchema.virtual("fullName").get(() => {
+  return this.firstName + " " + this.lastName;
+}).set((fullName) => {
+  const splitName = fullName.split(" ");
+  this.firstName = splitName[0] || "";
+  this.lastName = splitName[1] || "";
+});
+
+UserSchema.set("toJSON", { getters: true, virtuals: true });
 
 mongoose.model("User", UserSchema);
