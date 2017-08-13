b6059f00a84ad937927af3fcb77a0000de5f2629
moeabdol <mohd.a.saed@gmail.com>
2017-08-13 18:59:01 +0300

    Modify user controller and create signup and signin views

--- app/controllers/users.server.controller.js
+++ app/controllers/users.server.controller.js
@@ -1,4 +1,75 @@
-const User = require("mongoose").model("User");
+const User     = require("mongoose").model("User");
+const passport = require("passport");
+
+function getErrorMessage(err) {
+  let message = "";
+
+  if (err.code) {
+    switch (err.code) {
+      case 11000:
+      case 11001:
+        message = "Username already exists";
+        break;
+      default:
+        message = "Something went wrong";
+    }
+  } else {
+    for (let errName in err.errors) {
+      if (err.errors[errName].message) message = err.errors[errName].message;
+    }
+  }
+
+  return message;
+}
+
+exports.renderSignin = (req, res) => {
+  if (!req.user) {
+    res.render("signin", {
+      title: "Sign-in Form",
+      messages: req.flash("error") || req.flash("info")
+    });
+  } else {
+    return res.redirect("/");
+  }
+};
+
+exports.renderSignup = (req, res) => {
+  if (!req.user) {
+    res.render("signup", {
+      title: "Sign-up Form",
+      messages: req.flash("error")
+    });
+  } else {
+    return res.redirect("/");
+  }
+};
+
+exports.signup = (req, res, next) => {
+  if (!req.user) {
+    const user = new User(req.body);
+    user.provider = "local";
+
+    user.save((err) => {
+      if (err) {
+        const message = getErrorMessage(err);
+
+        req.flash("error", message);
+        return res.redirect("/signup");
+      }
+      req.login(user, (err) => {
+        if (err) return next(err);
+        return res.redirect("/");
+      });
+    });
+  } else {
+    return res.redirect("/");
+  }
+};
+
+exports.signout = (req, res) => {
+  req.logout();
+  res.redirect("/");
+};
 
 exports.create = (req, res, next) => {
   const user = new User(req.body);
