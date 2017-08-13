87f119abef2fb3a0a1b95845921970dad5f5e01b
moeabdol <mohd.a.saed@gmail.com>
2017-08-13 15:08:16 +0300

    Adapting user model

--- app/models/user.server.model.js
+++ app/models/user.server.model.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const crypto   = require("crypto");
+
 const Schema = mongoose.Schema;
 
 mongoose.Promise = global.Promise;
@@ -8,46 +10,32 @@ const UserSchema = new Schema({
   lastName:   String,
   email: {
     type:     String,
-    index:    true,
     match:    /.+@.+\..+/
   },
   username: {
     type:     String,
-    trim:     true,
     unique:   true,
-    required: true
+    required: "Username is required",
+    trim:     true
   },
   password: {
     type:     String,
     validate: [
       (password) => {
         return password.length >= 6;
-      },
-      "Password should be longer"
+      }, "Password should be longer"
     ]
   },
+  salt : String,
+  provider: {
+    type: String,
+    required: "Provider is required"
+  },
+  providerId: String,
+  providerData: {},
   created: {
     type:     Date,
     default:  Date.now
-  },
-  website: {
-    type:     String,
-    get: (url) => {
-      if (!url) {
-        return url;
-      } else {
-        if (url.indexOf("http://") !== 0 && url.indexOf("https://") !== 0) {
-          url = "http://" + url;
-        }
-        return url;
-      }
-    }
-  },
-  role: {
-    type:     String,
-    default:  "User",
-    enum: ["Admin", "Owner", "User"],
-    required: true
   }
 });
 
@@ -58,6 +46,38 @@ UserSchema.virtual("fullName").get(function() {
   this.firstName = splitName[0] || "";
   this.lastName = splitName[1] || "";
 });
+
+UserSchema.pre("save", function(next) {
+  if (this.password) {
+    this.salt = new Buffer(crypto.randomBytes(16).toString("base64"),
+      "base64");
+    this.password = this.hashPassword(this.password);
+  }
+  next();
+});
+
+UserSchema.methods.hashPassword = function(password) {
+  return crypto.pbkdf2Sync(password, this.salt, 10000, 64).toString("base64");
+};
+
+UserSchema.methods.authenticate = function(password) {
+  return this.password === this.hashPassword(password);
+};
+
+UserSchema.statics.findUniqueUsername = function(username, suffix, callback) {
+  const possibleUsername = username + (suffix || "");
+  this.findOne({ username: possibleUsername }, (err, user) => {
+    if (!err) {
+      if (!user) {
+        callback(possibleUsername);
+      } else {
+        return this.findUniqueUsername(username, (suffix || 0) + 1, callback);
+      }
+    } else {
+      callback(null);
+    }
+  });
+};
 
 UserSchema.set("toJSON", { getters: true, virtuals: true });
 
